584e1c6e879c497103b913c31458acf4dda66efb
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-13 01:31:21 -0500

    feat: add a chart in movements

--- src/features/movements/MovementsContainer.tsx
+++ src/features/movements/MovementsContainer.tsx
@@ -1,25 +1,40 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import React, { useEffect } from 'react';
 import { MovementsTable } from './components/MovementsTable';
 import { useGetMovements } from './hooks/useGetMovements';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
+import { BudgetVsExpensesChart } from './components/BudgetVsExpensesChart';
+import { useGetRubrosSum } from '../rubros/hooks';
+import { useGetMovementsSum } from './hooks';
 
 export default function MovementsContainer() {
   const project = useSelector((state: RootState) => state.project);
   const { getMovements, isLoading, movements } = useGetMovements(project.projectId);
+  const { getRubrosSum, totalValueSgr } = useGetRubrosSum(project.projectId);
+  const { getMovementsSum, totalAmount } = useGetMovementsSum(project.projectId);
 
   useEffect(() => {
     getMovements();
+    getRubrosSum();
+    getMovementsSum();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Container maxWidth={false}>
       <Grid container spacing={1}>
-        <Grid size={{ xs: 12 }}>
+        {/* Grid para la Tabla de Movimientos */}
+        <Grid size={{ xs: 12, md: 8 }}>
           <MovementsTable movements={movements} isLoading={isLoading} />
+        </Grid>
+
+        {/* Grid para el Gráfico de Presupuesto vs Gastos */}
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Box sx={{ height: '100%' }}>
+            <BudgetVsExpensesChart presupuesto={totalValueSgr} gastos={totalAmount} />
+          </Box>
         </Grid>
       </Grid>
     </Container>
